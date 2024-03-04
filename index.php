a6432d088007de6471f41323ad92f01185804aef
pedrogeloagachando <Pedro.2022311851@aluno.iffar.edu.br>
2024-03-04 16:55:15 -0300

    id, email e senha

--- index.php
+++ index.php
@@ -5,7 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body> 
+<body>
+    <table>
+         <thead>
+        <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Opções</th>
+</tr>
+</thead>
+</table>
+
    Óla mundo
+   <form method="post">
+
 </body>
+</form>
 </html>
